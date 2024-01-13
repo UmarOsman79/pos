bdebfc9b9edc7f01930f54a5b5ac1b4699f317d8
Umar Osman <umarosman77@gmail.com>
2024-01-13 09:19:46 +0200

    pom.xml - Added h2 database dependency
    application.properties - Added spring.datasource properties
    
    data.sql - Added file, dummy data in there for now
    
    UserEntity.java - Added @Table annotation

--- src/main/java/gmail/umarosman77/pos/entities/UserEntity.java
+++ src/main/java/gmail/umarosman77/pos/entities/UserEntity.java
@@ -3,8 +3,10 @@ package gmail.umarosman77.pos.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "user")
 public class UserEntity {
 
 	@GeneratedValue(strategy= GenerationType.AUTO)
